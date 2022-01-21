85a86d89de3020447cad488c3663f3bcd78ab42e
Mohammad Abdul Ghafour <mohammad1478971539@gmail.com>
2022-01-21 22:28:39 +0200

    Adding List to AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,21 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    List arr = [];
     return MaterialApp(
         theme: ThemeData(
           brightness: Brightness.dark,
         ),
         home: Scaffold(
           appBar: AppBar(
-            title: const Text("WordPair"),
+            title: const Text("My First App"),
             backgroundColor: Colors.amberAccent[400],
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.list),
+                onPressed: () => {},
+              )
+            ],
           ),
           body: const Center(
             child: Text('Hello World'),
